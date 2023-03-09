458e2bad5d9cbe6e2d0c73856123971db3f5b7fe
darkhowk <darkhowk.dark@gmail.com>
2023-03-09 16:26:28 +0900

    설정>메뉴 추가

--- TravalERP/TravalERP/myapp/setting/urls.py
+++ TravalERP/TravalERP/myapp/setting/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "setting"
 
 urlpatterns = [    
+    path("", views.index.as_view(), name="메인페이지"),
     path("setting/", views.settingIndex.as_view(), name="설정"), 
+    path("setting/menu", views.settingMenu.as_view(), name="메뉴관리"), 
 ]
--- TravalERP/TravalERP/myapp/setting/views.py
+++ TravalERP/TravalERP/myapp/setting/views.py
@@ -4,6 +4,26 @@ from django.views import generic
 from .models import Menu, Users
 # Create your views here.
 
+class index(generic.ListView):
+   def __init__(self):
+        self.title_nm = "TON TOUR"
+        self.ogImgUrl = ""
+        self.descript = "TON TOUR 페이지입니다"
+        self.template_name = "index.html"
+        self.topMenu = Menu.objects.filter(menu_type="TOP")
+        self.leftMenu = Menu.objects.filter(menu_type="LEFT")
+      
+   def get(self, request, *args, **kwargs):
+      self.content = {
+                        "descript" : self.descript,
+                        "title_nm" : self.title_nm,
+                        "ogImgUrl" : self.ogImgUrl,
+                        "topMenu"  : self.topMenu,
+                        "leftMenu"  : self.leftMenu
+                     }
+
+      return render(request, self.template_name, self.content)
+
 class settingIndex(generic.ListView):
    def __init__(self):
         self.title_nm = "설정"
@@ -23,3 +43,26 @@ class settingIndex(generic.ListView):
                      }
 
       return render(request, self.template_name, self.content)
+
+class settingMenu(generic.ListView):
+   def __init__(self):
+        self.title_nm = "설정>메뉴관리"
+        self.ogImgUrl = ""
+        self.descript = "메뉴관리 페이지입니다"
+        self.template_name = "setting/menu.html"
+        self.topMenu = Menu.objects.filter(menu_type="TOP")
+        self.leftMenu = Menu.objects.filter(menu_type="LEFT")
+        self.Menu = Menu.objects.all()
+      
+   def get(self, request, *args, **kwargs):
+      self.content = {
+                        "descript" : self.descript,
+                        "title_nm" : self.title_nm,
+                        "ogImgUrl" : self.ogImgUrl,
+                        "topMenu"  : self.topMenu,
+                        "leftMenu"  : self.leftMenu,
+                        "Menu" : self.Menu
+                     }
+
+      return render(request, self.template_name, self.content)
+      
