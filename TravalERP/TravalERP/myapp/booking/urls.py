a5a3de6a3582077ba0cb8132b08ba26b0cae35d0
darkhowk <darkhowk.dark@gmail.com>
2023-03-14 15:25:34 +0900

    소스 꼬인것 풀기

--- TravalERP/TravalERP/myapp/booking/urls.py
+++ TravalERP/TravalERP/myapp/booking/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("booking/", views.bookingIndex.as_view(), name="수배내역"), 
     path("booking/add/", views.bookingAdd.as_view(), name="수배추가"), 
     path("booking/add2/", views.bookingAdd2.as_view(), name="수배추가2"), 
+     path("booking/addModify/", views.bookingAddModify.as_view(), name="수배추가2"), 
 ]
--- TravalERP/TravalERP/myapp/booking/views.py
+++ TravalERP/TravalERP/myapp/booking/views.py
@@ -43,13 +43,26 @@ class bookingAdd2(generic.ListView):
    def __init__(self):
       self.title_nm = "수배추가"
       self.ogImgUrl = ""
-<<<<<<< HEAD
-      self.descript = "수배추가 페이지입니다"
-      self.template_name = "booking/add_modify.html"
-=======
       self.descript = "수배추가 테스트페이지입니다"
       self.template_name = "booking/add2.html"
->>>>>>> 8b0b71e0c021a206109114fa10a9efee804518fe
+      self.topMenu = Menu.objects.filter(menu_type="TOP")
+
+   def get(self, request, *args, **kwargs):
+      self.content = {
+                        "descript" : self.descript,
+                        "title_nm" : self.title_nm,
+                        "ogImgUrl" : self.ogImgUrl,
+                        "topMenu"  : self.topMenu
+                     }
+
+      return render(request, self.template_name, self.content)
+   
+class bookingAddModify(generic.ListView):
+   def __init__(self):
+      self.title_nm = "수배추가"
+      self.ogImgUrl = ""
+      self.descript = "수배추가 테스트페이지입니다"
+      self.template_name = "booking/add_modify.html"
       self.topMenu = Menu.objects.filter(menu_type="TOP")
 
    def get(self, request, *args, **kwargs):
