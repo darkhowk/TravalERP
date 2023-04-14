ab025cab34036de2dc014a1e189329a29887dce0
aora5 <aora5@naver.com>
2023-04-14 10:56:01 +0900

    rooming add 최종

--- TravalERP/TravalERP/myapp/rooming/views.py
+++ TravalERP/TravalERP/myapp/rooming/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
-from ..common.common_models import Menu
+from django.http import JsonResponse, HttpResponse
+from ..common.common_models import * 
+import json
 # Create your views here.
 
 class roomingIndex(generic.ListView):
@@ -26,16 +28,23 @@ class roomingAdd(generic.ListView):
       self.title_nm = "루밍추가"
       self.ogImgUrl = ""
       self.descript = "루밍추가 페이지입니다"
-      self.template_name = "rooming/add.html"
+      self.template_name = "rooming/add.html" 
       self.topMenu = Menu.objects.filter(menu_type="TOP")
 
    def get(self, request, *args, **kwargs):
+      tourconductor = Tourconductor.objects.filter(use_yn ='Y')
+      optionData = {'tourconductor': tourconductor}
       self.content = {
                         "descript" : self.descript,
                         "title_nm" : self.title_nm,
                         "ogImgUrl" : self.ogImgUrl,
-                        "topMenu"  : self.topMenu
+                        "topMenu"  : self.topMenu,
+                        "optionData" : optionData,
                      }
 
       return render(request, self.template_name, self.content)
    
+def pathtoMode(path):
+   if path == 'tourconductor':
+      Models = Tourconductor
+   return Models
