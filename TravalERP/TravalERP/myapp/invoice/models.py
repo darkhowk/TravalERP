f27b589874d080894830bbe479239b594ca650b1
darkhowk <darkhowk.dark@gmail.com>
2023-04-18 09:41:09 +0900

    모델 기본셋팅

--- TravalERP/TravalERP/myapp/invoice/models.py
+++ TravalERP/TravalERP/myapp/invoice/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from ..common.common_models import commonModel
+from ..booking.models import BookingMaster
 
 # Create your models here.
+class InvoiceMaster(commonModel):
+    
+    class Meta:
+        managed = False
+        db_table = 'invoice_master'
+        verbose_name = '인보이스 마스터'
+        verbose_name_plural = '인보이스 마스터 목록'       
+
+class InvoiceDetail(commonModel):
+  
+  
+    class Meta:
+        managed = False
+        db_table = 'invoice_detail'
+        verbose_name = '인보이스 디테일'
+        verbose_name_plural = '인보이스 디테일 목록'      
+
+
